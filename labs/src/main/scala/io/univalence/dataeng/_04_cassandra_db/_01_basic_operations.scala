2018c89f5f5e44f8d552d3a41276fd5298d50205
François Sarradin <fsarradin@gmail.com>
2022-03-17 17:49:03 +0100

    typo

--- labs/src/main/scala/io/univalence/dataeng/_04_cassandra_db/_01_basic_operations.scala
+++ labs/src/main/scala/io/univalence/dataeng/_04_cassandra_db/_01_basic_operations.scala
@@ -13,51 +13,56 @@ import java.time.LocalDate
 
 /**
  * =Cassandra=
- * We saw in-process databases such as RocksDB and MapDB. However this
- * kind of databases doesn't scala when we need to deal with tons of
+ * We saw in-process databases such as RocksDB and MapDB. However, this
+ * kind of databases doesn't scale when we need to deal with tons of
  * data.
  *
- * Cassandra is a column oriented, No SQL database with eventual
- * consistency. (meaning that they chose Availability and Partition
- * tolerance in the CAP theorem).
+ * Cassandra is a column-oriented NoSQL database with eventual
+ * consistency (meaning that it is focused on Availability and Partition
+ * tolerance, following the CAP theorem).
  *
- * Cassandra is design to handle a high volume of data by scaling using
- * multiple nodes with no single point of failure. Thus, it is design as
- * a multi master system and not a master/slave system. It means that
- * any nodes can die without impacting directly the system.
+ * Cassandra is designed to handle a high volume of data by scaling in
+ * using multiple nodes with no single point of failure (SPOF). Thus, it
+ * is designed as a multi master system and not a master/slave system.
+ * It means that some nodes can die without impacting directly the
+ * system, so, operators have time replace dead nodes.
  *
  * Cassandra has first been created in Facebook offices and is now an
- * open source project under the Apache foundation. Cassandra is written
- * in C++ and comes with a Java wrapper.
+ * open source project under the Apache foundation, mainly managed by
+ * the company Datastax. Cassandra is written in Java.
  *
  * ==Concepts==
- * Cassandra is build around different concepts:
- *   - Keyspace: Configurations containing tables and defining how many
- *     times we should replicate the data for example.
- *   - Table: Defines the typed schema for a collection of partitions.
- *   - Partition:Defines the mandatory part of the primary key all rows
- *     in Cassandra must have to identify the node in a cluster where
- *     the row is stored.
- *   - Row: Collection of columns identifier by a primary key
- *   - Column: A single column with a type for example (name: String)
+ * Cassandra is built around different concepts:
+ *   - '''Keyspace''': Configurations containing tables and defining how
+ *     many times we should replicate the data for example.
+ *   - '''Table''': Defines the typed schema for a collection of
+ *     partitions.
+ *   - '''Partition''': Defines the mandatory part of the primary key
+ *     all rows in Cassandra must have to identify the node in a cluster
+ *     where the row is stored.
+ *   - '''Row''': Collection of columns identified by a primary key.
+ *   - '''Column''': A single column with a type for example (name:
+ *     String). It is possible to use composite types like lists, maps,
+ *     sets, and any kind of sub-structures.
  *
  * ==Data storage==
- * Cassandra also uses the same data storage as RocksDB following the
- * LSM-tree for write heavy databases.
+ * Cassandra also uses the same type of data storage as RocksDB. It is
+ * also based on LSM-tree, which enhances huge amount of writes in the
+ * database.
  *
  * ==Cassandra specificity==
- * Cassandra differ a lot from SQL database because it is query first.
+ * Cassandra differs a lot from SQL database because it is query first.
  * It means that we design Cassandra table with queries in mind. Indeed
  * in Cassandra you can't join tables and you can query the data only
  * using the partition keys. If you need to query the same kind of data
- * in two different manners, they you denormalize the data and you
+ * in two different manners, then you denormalize the data and you
  * create two tables with different partition key.
  *
- * Also even if you can use ORDER BY to order your data, it is advised
+ * Also, even if you can use ORDER BY to order your data, it is advised
  * to use your clustering key to sort your data.
  *
  * ==In this file==
- *   - The code is wrap around a TestContainer creating the Cassandra
+ *   - The code is wrapped around a TestContainer creating the Cassandra
  *     container to interact with.
  *   - You then have an exercise to interact with Cassandra.
  */
@@ -72,7 +77,9 @@ object _01_basic_operations {
       session.execute(statement)
     }
 
+    /** FIXME explain */
     usingCassandra { session =>
+      /** FIXME remove implicits */
       implicit val implicitSession: CqlSession = session
       implicit val implicitKeySpace: String    = cassandraKeyspace
 
@@ -81,15 +88,21 @@ object _01_basic_operations {
         /**
          * The beginning of a Cassandra journey is creating a keyspace,
          * since we have only one node, we ask the system to replicate
-         * the data only once.
+         * the data only once. The keyspace will be used to then create
+         * tables.
          */
-        val keyspaceQuery = SchemaBuilder.createKeyspace(cassandraKeyspace).ifNotExists.withSimpleStrategy(1)
+        val keyspaceQuery =
+          SchemaBuilder
+            .createKeyspace(cassandraKeyspace)
+            .ifNotExists
+            .withSimpleStrategy(1)
+
         session.execute(keyspaceQuery.build)
 
         /**
-         * We can use the newly create keyspace by default. Here we use
-         * CQL, CQL stands for Cassandra Query Language it is a SQL like
-         * language to interact with Cassandra.
+         * We can use the newly created keyspace by default. Here, we
+         * use CQL. CQL stands for ''Cassandra Query Language''. It is a
+         * SQL-like language designed to interact with Cassandra.
          */
         session.execute("USE " + CqlIdentifier.fromCql(cassandraKeyspace))
       }
@@ -111,13 +124,14 @@ object _01_basic_operations {
             .withPartitionKey("video_id", DataTypes.INT)
             .withColumn("title", DataTypes.TEXT)
             .withColumn("creation_date", DataTypes.DATE)
+
         executeStatement(createTable.build)
       }
 
       exercise("Insert a video") {
 
         /** We now want to insert a video in our newly create table */
-        val video: Video = Video(1, "My video", LocalDate.now())
+        val video: Video = Video(1, "My video", LocalDate.now(), "Jon")
 
         val insertInto: RegularInsert =
           QueryBuilder
@@ -136,6 +150,7 @@ object _01_basic_operations {
             .setString(1, video.title)
             .setLocalDate(2, video.date)
             .setString(3, video.creator)
+
         session.execute(statement)
       }
 
